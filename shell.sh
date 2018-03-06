10344366823c965298eec1f3f4a804f695786101
root <root@localhost.localdomain>
2018-03-06 04:37:29 -0500

    fourth commit

--- shell.sh
+++ shell.sh
@@ -8,6 +8,10 @@ ssh-keygen
 cat /root/.ssh/id_rsa.pub
 # 克隆远程仓库到本地
 git clone git@github.com:coldly/Operations.git
+# 添加文件
+git add shell.sh
+# 提交修改
+git commit -m "second commit"
 # 第一次提交
 git push -u origin master
 
