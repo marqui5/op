e357083135c731db1c13ec76cfd308244808fc45
root <root@localhost.localdomain>
2018-03-09 12:23:05 -0500

    iptables

--- shell.sh
+++ shell.sh
@@ -1,9 +1,72 @@
+#!/bin/bash
+# 编写shell脚本循环创建100个用户（user_[0-99])
+#!/bin/bash
+for((i = 0; i < 100; i++));
+do
+useradd user_$i;
+done
+# 统计shell.sh文件的行数
+wc -l shell.sh
 # 统计cpu占用前十的进程
 ps aux | sort -k3 | head -10
-# 将/opt目录下包含'hello'的文件替换成'hi'
-sed -i 's/hellohhhhh/hi/g' `grep -rl 'hellohhhhh' /opt`
-# awk
+# 开机启动mysql
+chkconfig mysqld on
+# centos7
+systemctl enable mariadb
+# 查看22端口现在运行的情况
+lsof -i :22
+COMMAND  PID USER   FD   TYPE DEVICE SIZE/OFF NODE NAME
+ssh     4169 root    3u  IPv4 127672      0t0  TCP 172.16.123.1:52650->172.16.123.128:ssh (ESTABLISHED)
+ssh     4172 root    3u  IPv4 130231      0t0  TCP 172.16.123.1:59818->172.16.123.129:ssh (ESTABLISHED)
+# 配置防火墙只允许远程主机访问本机的80端口
+/sbin/iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+/etc/rc.d/init.d/iptables save
+/etc/init.d/iptables status
+# centos7 firewall
+1、firewalld的基本使用
+启动： systemctl start firewalld
+查看状态： systemctl status firewalld
+停止： systemctl disable firewalld
+禁用： systemctl stop firewalld
 
+2.systemctl是CentOS7的服务管理工具中主要的工具，它融合之前service和chkconfig的功能于一体。
+启动一个服务：systemctl start firewalld.service
+关闭一个服务：systemctl stop firewalld.service
+重启一个服务：systemctl restart firewalld.service
+显示一个服务的状态：systemctl status firewalld.service
+在开机时启用一个服务：systemctl enable firewalld.service
+在开机时禁用一个服务：systemctl disable firewalld.service
+查看服务是否开机启动：systemctl is-enabled firewalld.service
+查看已启动的服务列表：systemctl list-unit-files|grep enabled
+查看启动失败的服务列表：systemctl --failed
+
+3.配置firewalld-cmd
+查看版本： firewall-cmd --version
+查看帮助： firewall-cmd --help
+显示状态： firewall-cmd --state
+查看所有打开的端口： firewall-cmd --zone=public --list-ports
+更新防火墙规则： firewall-cmd --reload
+查看区域信息:  firewall-cmd --get-active-zones
+查看指定接口所属区域： firewall-cmd --get-zone-of-interface=eth0
+拒绝所有包：firewall-cmd --panic-on
+取消拒绝状态： firewall-cmd --panic-off
+查看是否拒绝： firewall-cmd --query-panic
+那怎么开启一个端口呢
+添加
+firewall-cmd --zone=public --add-port=80/tcp --permanent    （--permanent永久生效，没有此参数重启后失效）
+重新载入
+firewall-cmd --reload
+查看
+firewall-cmd --zone= public --query-port=80/tcp
+删除
+firewall-cmd --zone= public --remove-port=80/tcp --permanent
+
+# 将/opt目录下包含'hellohhhhh'的文件替换成'hi'
+sed -i 's/hellohhhhh/hi/g' `grep -rl 'hellohhhhh' /opt`
+# 将shell.sh中/var/log 替换成/usr/local
+sed -i 's/\/var\/log/\/usr\/local/g' shell.sh
+# awk
+awk 'BEGIN{for(i=1; i<=10; i++) print i}'
 # kill某个用户的所有进程
 pkill -u user
 killall -u user
