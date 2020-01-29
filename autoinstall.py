9c255ee7ccbe3d0d3987325dc518e967588a42a6
coldly <cold1y@163.com>
2020-01-30 02:05:12 +0800

    apt-get autoinstall

---
+++ autoinstall.py
@@ -0,0 +1,17 @@
+import os
+auto = os.popen('apt-get install')
+#print(auto.read())
+lines = auto.readlines()
+#print(lines[5:-2])
+install = []
+for line in lines[5:-2]:
+    linedata = line.split(' ')
+    for i in linedata:
+        if(i != ''):
+            install.append(i.replace('\n', ''))
+print(install)
+q = input(str(len(install)) + ' package will be set to manually installed.\nDo you want to continue? [Y/n]')
+if(q == 'Y'):
+    for autoinstall in install:
+        out = os.popen('apt-get install ' + autoinstall)
+        print(out.read())
